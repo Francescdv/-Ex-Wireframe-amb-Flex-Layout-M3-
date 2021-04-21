6ecdac7273a8c02fc0e3cbf0e2a475f9bac1c4fe
francescdv <francescdv@gmail.com>
2021-04-21 09:42:25 +0200

    lv3

---
+++ lv3/main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('header',{ delay: 300}); 
+ScrollReveal().reveal('.contenidor-art',{ delay: 500}); 
+ScrollReveal().reveal('.contenidor-art2',{ delay: 500}); 
+ScrollReveal().reveal('footer',{ delay: 300}); 
